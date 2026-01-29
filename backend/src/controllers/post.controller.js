7ff1413250dd4494397eb1ff2f7721f5c6aef4a2
Erfan91 <sadidhadix@gmail.com>
2026-01-29 17:07:41 +0100

    post img upload added

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,4 +1,6 @@
 import { Post } from "../models/post.model.js";
+import multer from "multer";
+import path from "path";
 
 const createPost = async (req, res, next) => {
 
@@ -52,10 +54,35 @@ const deletePost = async (req, res ,next) => {
     });
 }
 
+let imageName = "";
+const storage = multer.diskStorage({
+    destination: path.join("./images"),
+    filename: function (req, file, cb) {
+        imageName = Date.now() + path.extname(file.originalname);
+        cb(null, imageName)
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 3000000 }, // 3MB limit
+}).single("myImage");
+
+
+const uploadImage = (req, res, next) => {
+    upload(req, res, function (err) {
+        if (err) {
+            res.status(500).json({ message: "Image upload failed", error: err.message });
+        } else {
+            res.status(201).json({ url: "http://localhost:3001/images/" + imageName });
+        }
+    })
+}
 
 export {
     createPost,
     getPost,
     updatePost,
-    deletePost
+    deletePost,
+    uploadImage
 }
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {createPost, getPost, updatePost, deletePost} from '../controllers/post.controller.js';
+import {createPost, getPost, updatePost, deletePost, uploadImage} from '../controllers/post.controller.js';
 const router = Router();
 
 router.route("/create-post").post(createPost);
+router.route("/upload-image").post(uploadImage)
 router.route("/get-post").get(getPost);
 router.route("/update-post/:id").patch(updatePost);
 router.route("/delete-post/:id").delete(deletePost);
