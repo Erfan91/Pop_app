6921452a4ebc58a668c94cdc2915c6d37ee90ced
Erfan91 <sadidhadix@gmail.com>
2026-01-29 13:08:18 +0100

    added some additional props to user model such as mood

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -9,13 +9,20 @@ const postSchema = new mongoose.Schema(
 
         decription: String,
 
-        content : String,
+        content : [String],
+
+        //  mood is something new, this effect is chosen by user when creating post,
+        //  then the mood animation appears seconds before the content to other 
+        //  users to express the mood they are in then the content/photo/video/text appears  
+        mood: [String],
          
         likes : {
             type: [mongoose.Types.ObjectId],
             ref: 'User'
         },
 
+
+        // users could comment back with mood anims already created, these comments hold a block section to appear on top of eachother like showroom
         comments: {
             type: [mongoose.Types.ObjectId],
             ref: "User"
